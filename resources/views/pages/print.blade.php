c40e890b59e6fa2a24513eb5882666ba378d61b5
dwikiardi <dwiardi60@gmail.com>
2022-08-17 17:41:01 +0800

    Fix Font Size & color

--- resources/views/pages/print.blade.php
+++ resources/views/pages/print.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.app', ['activePage' => 'printTable', 'titlePage' => __('Print')])
-<link href="{{ asset('assets/css/tableInvoice.css') }}" rel="stylesheet" type="text/css" >
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 @section('content')
@@ -9,7 +8,7 @@
             <div class="col-12 col-lg-12">
                 <div class="row">
                     <div class="col-12">
-                        <div class="text-center text-150">
+                        <div>
                             <img src="{{ asset('assets/img/header/header.jpeg') }}">
                         </div>
                     </div>
@@ -18,89 +17,89 @@
                         <div class="row">
                             <div class="col-sm-6">
                                     <div>
-                                        <span class="text-sm text-grey-m2 align-middle">Kepada  :</span>
-                                        <span class="text-600 text-110 text-black align-middle">{{$header[0]['data_nama']}}</span>
+                                        <span class="text-150 text-sm text-dark align-middle">Kepada  :</span>
+                                        <span class="text-150 text-dark align-middle">{{$header[0]['data_nama']}}</span>
                                     </div>
                                     <div>
-                                        <span class="text-sm text-grey-m2 align-middle">UP  :</span>
+                                        <span class="text-150 text-sm text-dark align-middle">UP  :</span>
                                         @if($header[0]['data_up'] == null)
-                                            <span class="text-600 text-110 text-black align-middle">
+                                            <span class="text-150 text-dark align-middle">
                                                 -
                                             </span>
                                         @else
-                                            <span class="text-600 text-110 text-black align-middle">
+                                            <span class="text-150 text-dark align-middle">
                                                 {{$header[0]['data_up']}}
                                             </span>
                                         @endif
                                     </div>
                                     <div>
-                                        <span class="text-sm text-grey-m2 align-middle">Alamat  :</span>
-                                        <span class="text-600 text-110 text-black align-middle">{{$header[0]['data_alamat']}}</span>
+                                        <span class="text-150 text-sm text-dark align-middle">Alamat  :</span>
+                                        <span class="text-150 text-dark align-middle">{{$header[0]['data_alamat']}}</span>
                                     </div>
                                 </div>
-                                <div class="text-95 col-sm-6 align-end d-sm-flex justify-content-end">
+                                <div class="text-150 col-sm-6 align-end d-sm-flex justify-content-end">
                                     <hr class="d-sm-none" />
-                                    <div class="text-grey-m2">
-                                        <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125 text-right">
+                                    <div>
+                                        <div class="mt-1 mb-2 text-dark text-right">
                                             Gianyar, {{date('d-m-Y')}}
                                         </div>
-                                        <div class="mt-6 mb--4 text-secondary-m1 text-600 text-125 text-right">
+                                        <div class="mt-6 mb--4 text-dark text-right">
                                             Nomor Invoice : IN{{$nomer}}{{date('d-m-Y')}}
                                         </div>
                                     </div>
                             </div>
                         </div>
-                        <div class="border border-white text-center">
+                        <div class="border border-white text-center text-dark text-60">
                             INVOICE
                         </div>
                         <div class="table-responsive">
                             <table class="table table-striped table-bordered">
                                 <thead>
-                                    <tr class="text-center">
-                                        <th>No</th>
-                                        <th>KETERANGAN</th>
-                                        <th>HARGA (Rp) SATUAN</th>
-                                        <th>QTY</th>
-                                        <th>Satuan</th>
-                                        <th>TOTAL</th>
+                                    <tr class="text-center text-dark">
+                                        <th class="text-120">No</th>
+                                        <th class="text-120">KETERANGAN</th>
+                                        <th class="text-120">HARGA (Rp) SATUAN</th>
+                                        <th class="text-120">QTY</th>
+                                        <th class="text-120">Satuan</th>
+                                        <th class="text-120">TOTAL</th>
                                     </tr>
                                 </thead>
-                                <tbody class="text-95 text-secondary-d3 text-center">
+                                <tbody class="text-center text-dark">
                                     @foreach ($data as $key => $value)
                                         <tr></tr>
                                         <tr>
-                                            <td>{{$key+1}}</td>
-                                            <td>{{$value['nama_barang']}}</td>
-                                            <td>Rp {{number_format($value['harga_barang'],0,',','.')}}</td>
-                                            <td class="text-95">{{$value['jumlah_barang']/$value['harga_barang']}}</td>
-                                            <td class="text-95">{{$value['satuan_barang']}}</td>
-                                            <td class="text-secondary-d2">Rp {{number_format($value['jumlah_barang'],0,',','.')}}</td>
+                                            <td class="text-120">{{$key+1}}</td>
+                                            <td class="text-120">{{$value['nama_barang']}}</td>
+                                            <td class="text-120">Rp {{number_format($value['harga_barang'],0,',','.')}}</td>
+                                            <td class="text-120">{{$value['jumlah_barang']/$value['harga_barang']}}</td>
+                                            <td class="text-120">{{$value['satuan_barang']}}</td>
+                                            <td class="text-120">Rp {{number_format($value['jumlah_barang'],0,',','.')}}</td>
                                         </tr>
                                         @endforeach
                                 </tbody>
-                                <tfoot>
+                                <tfoot class="text-120 text-center text-dark">
                                     @foreach ($footer as $data)
                                     <tr>
-                                        <th colspan="5" style="text-align:right">Sub Total :</th>
+                                        <th class="text-120" colspan="5" style="text-align:right">Sub Total :</th>
                                         <th>{{$data['data_sum']}}</th>
                                     </tr>
                                     <tr>
-                                        <th colspan="5" style="text-align:right">Down Payment :</th>
+                                        <th class="text-120" colspan="5" style="text-align:right">Down Payment :</th>
                                         <th>{{$data['data_dp']}}</th>
                                     </tr>
                                     <tr>
-                                        <th colspan="5" style="text-align:right">Disc :</th>
+                                        <th class="text-120" colspan="5" style="text-align:right">Disc :</th>
                                         <th>{{$data['data_disc']}}</th>
                                     </tr>
                                     <tr>
-                                        <th colspan="5" style="text-align:right">Total :</th>
+                                        <th class="text-120" colspan="5" style="text-align:right">Total :</th>
                                         <th>{{$data['data_total']}}</th>
                                     </tr>
                                     @endforeach
                                 </tfoot>
                             </table>
                         </div>
-                        <div class="container">
+                        <div class="container text-dark text-150">
                             <div class="row">
                               <div class="col-6">
                                 <div class="row">
@@ -138,7 +137,7 @@
                                   </div>
                                   <div class="row">
                                     <div class="col-sm">
-                                        <br> Diterima Oleh, <br> <br> <br>(&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;)
+                                        <br> Diterima Oleh, <br> <br> <br> <br> <br>(&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;)
                                     </div>
                                   </div>
                               </div>
@@ -155,7 +154,7 @@
                                 </div>
                                 <div class="row pl-9 ml-5">
                                     <div class="col-sm text-center">
-                                      <br> <br> <br> {{ auth()->user()->name }}
+                                      <br> <br> <br> <br> <br>{{ auth()->user()->name }}
                                     </div>
                                 </div>
                               </div>
